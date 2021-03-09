d7e0449c4c5c0c747c8950e94ae6f75d63058fd0
Milton-668 <miltonmjcp@gmail.com>
2021-03-08 23:02:46 -0300

    feat(controllers): Criado o metodo de inserção em postagens #4

--- src/main/java/com/blogpessoal2/blogpessoal2/controllers/PostagensController.java
+++ src/main/java/com/blogpessoal2/blogpessoal2/controllers/PostagensController.java
@@ -1,10 +1,13 @@
 package com.blogpessoal2.blogpessoal2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,23 +15,21 @@ import com.blogpessoal2.blogpessoal2.models.Postagens;
 import com.blogpessoal2.blogpessoal2.repository.PostagensRepository;
 
 @RestController
-@RequestMapping(value= "/postagens")
+@RequestMapping(value = "/postagens")
 @CrossOrigin("*")
 public class PostagensController {
 
-		@Autowired
-		private PostagensRepository repository;
-		
-		@GetMapping("/{id}")
-		public ResponseEntity<Postagens> getById(@PathVariable long id){
-			return repository.findById(id).map(obj-> ResponseEntity.ok(obj))
-					.orElse(ResponseEntity.notFound().build());
-		}
-		
-		
-		
-		
-		
-		
-		
+	@Autowired
+	private PostagensRepository repository;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagens> getById(@PathVariable long id) {
+		return repository.findById(id).map(obj -> ResponseEntity.ok(obj)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagens> post(@RequestBody Postagens postagens) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagens));
+	}
+
 }
